61d2216aefa55a2e8e46bedbc68b953296c2f2b1
昱緯 <csabc993611@Gmail.com>
2022-07-13 01:20:49 +0800

    test

---
+++ CorePractice_SwaggerService/CorePractice_SwaggerService/Implement/CovidService.cs
@@ -0,0 +1,32 @@
+﻿using CorePractice_SwaggerRepository.Interface;
+using CorePractice_SwaggerService.Dtos;
+using CorePractice_SwaggerService.Interface;
+
+namespace CorePractice_SwaggerService.Implement;
+
+public class CovidService:ICovidService
+{
+    private readonly ICovidRepository _covidRepository;
+
+    public CovidService(ICovidRepository covidRepository)
+    {
+        _covidRepository = covidRepository;
+    }
+    
+    public async Task<IEnumerable<CovidDto>> Get()
+    {
+        // 取得確診資訊
+        var entities = await _covidRepository.Get();
+
+        var dtos = entities.Select(x => new CovidDto
+        {
+            Date = x.Date,
+            DiagnosedCountOriginTW = x.DiagnosedCountOriginTW,
+            DiagnosedCountForeignTW = x.DiagnosedCountForeignTW,
+            DiagnosedCountOriginTaichung = x.DiagnosedCountOriginTaichung,
+            DiagnosedCountForeignTaichung = x.DiagnosedCountForeignTaichung
+        });
+
+        return dtos;
+    }
+}
